782dd39c0cd865fe14bf4986125d9e941aa975c4
PavloKonopadaDev <pavlokonopadadev@gmail.com>
2021-04-07 18:19:01 +0300

    created a few pages

--- conreality_menu/lib/main.dart
+++ conreality_menu/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:conreality_menu/pages/splashScreen/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,8 +9,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     
-      home:
+      debugShowCheckedModeBanner: false,
+      home: SplashScreenPage(),
+      routes: {
+        //"loginPage": (context) => (),
+        //"signIn": (context) => (),
+        //"signUp": (context) => (),
+        //"forgotPassword": (context) => (),
+        //"mainMenu": (context) => (),
+      },
     );
   }
 }
